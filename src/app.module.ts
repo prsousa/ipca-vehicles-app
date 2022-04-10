f6f6c79b54ff6efef0a86c70d2dc6b8fb3d998d0
Paulo Sousa <pcsousa@ipca.pt>
2022-04-10 15:03:48 +0100

    Add connection to mongodb
    - no auth credentials are being used
    - assumes "vehicles" DB is already created on mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { PositionsModule } from './positions/positions.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [VehiclesModule, PositionsModule],
+  imports: [
+    VehiclesModule,
+    PositionsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/vehicles'),
+  ],
 })
 export class AppModule {}
